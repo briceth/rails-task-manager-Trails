6e1b323b20f675cdbe85d25821d1d1bb3cc5bdc4
Brice Tessier Huort <tessierhuort@gmail.com>
2016-08-09 11:12:04 +0200

    add task dans db

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160809084614) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
